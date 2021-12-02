a47f629a3b07b78d284c285f12bbde806a856c09
Emir1996-GitHub <79993389+Emir1996-GitHub@users.noreply.github.com>
2021-12-02 15:59:10 +0300

    no.cs
    add an optional extended description...

---
+++ no.cs
@@ -0,0 +1,17 @@
+// you should write no
+
+using System;
+using System.Threading;
+
+namespace no{
+ class Program{
+  public static void Main(String[] Args){
+    Console.WriteLine("nonononononononononononono?");
+    Console.ReadLine();
+    string no = Console.ReadLine();
+    if (no != "no"){
+      Console.WriteLine((6*3-6+3+16+1943))
+    }
+  } 
+ }
+}
