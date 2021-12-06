9ad71cbcd81bbd73b474718173b6ee65d71507a2
Emir1996-GitHub <79993389+Emir1996-GitHub@users.noreply.github.com>
2021-12-06 16:53:15 +0300

    no2.cs

--- no2.cs
+++ no2.cs
@@ -1,5 +1,5 @@
-using System;
-using System.Threading;
+using System; // removes System from System.Console.ReadLine(); \\
+using System.Threading; // removes System from System.Thread.Sleep(ms); \\
 
 namespace no2{ 
  class Program{
@@ -13,6 +13,16 @@ namespace no2{
    Console.WriteLine("=");
    Thread.Sleep(50);
    Console.WriteLine((6*3));
-  }
+ }
+   public static void Mqin(String[] args){
+   bool Echo = true;
+   if(Echo == true){
+       Console.WriteLine("   6");
+       Console.WriteLine("   3");
+       Console.WriteLine("X");
+       Console.WriteLine("-------");
+       Console.WriteLine("   18");
+    }
+ }
  }
 }
